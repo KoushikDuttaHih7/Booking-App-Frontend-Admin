0155b41a7e2fda9769cb39131ec59d7a29149dc4
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-07-18 12:16:14 +0530

    Get and Delete Users

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,1 +1,2 @@
 export const auth_API = "http://localhost:8800/api/auth";
+export const user_API = "http://localhost:8800/api/user";
--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,14 +1,27 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
+import { userColumns } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { user_API } from "../../apis/api";
+import axios from "axios";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  // const location = useLocation()
+  // const path = location.pathname.split('/')[1]
+  const [list, setList] = useState([]);
+  const { data } = useFetch(user_API);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${user_API}/${id}`);
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {}
   };
 
   const actionColumn = [
@@ -18,13 +31,13 @@ const Datatable = () => {
       width: 200,
       renderCell: (params) => {
         return (
-          <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
-              <div className="viewButton">View</div>
+          <div className='cellAction'>
+            <Link to='/users/test' style={{ textDecoration: "none" }}>
+              <div className='viewButton'>View</div>
             </Link>
             <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
+              className='deleteButton'
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
@@ -34,20 +47,21 @@ const Datatable = () => {
     },
   ];
   return (
-    <div className="datatable">
-      <div className="datatableTitle">
+    <div className='datatable'>
+      <div className='datatableTitle'>
         Add New User
-        <Link to="/users/new" className="link">
+        <Link to='/users/new' className='link'>
           Add New
         </Link>
       </div>
       <DataGrid
-        className="datagrid"
-        rows={data}
+        className='datagrid'
+        rows={list}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+        getRowId={(row) => row._id}
       />
     </div>
   );
